11a3d20684f7b28e6d52a993131adb034b84ed4d
Josh Miller <jamiller13086@gmail.com>
2014-03-19 23:54:18 -0400

    Initial Commit

---
+++ wasabi_core/__init__.py
@@ -0,0 +1,3 @@
+import urllib2
+response = urllib2.urlopen('https://74.125.226.80')
+print response.read();
---
+++ wasabi_core/util.py
@@ -0,0 +1,11 @@
+'''
+Created on Mar 19, 2014
+
+@author: josh23941
+'''
+
+import iptools
+
+def inc_ip (ip):
+    return iptools.ipv4.ip2long(ip)
+
