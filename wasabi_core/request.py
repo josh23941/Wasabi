0af0528394886a74e74b351db0822a17d51bcee6
Josh Miller <jamiller13086@gmail.com>
2014-03-20 23:52:02 -0400

    Added stdout logging of request/response pairs

--- wasabi_core/request.py
+++ wasabi_core/request.py
@@ -3,16 +3,25 @@ Created on Mar 20, 2014
 
 @author: josh23941
 '''
+import urllib2
+import logging
 
-class Request(object):
-    '''
-    @todo: 
-    '''
+'''
+Default Headers
+'''
+DEFAULT_HEADERS = {'User-Agent' : 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:27.0) Gecko/20100101 Firefox/27.0',
+                   'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                   'Accept-Language' : 'en-US,en;q=0.5',
+                   'Accept-Encoding' : 'gzip,deflate'}
 
-
-    def __init__(self):
-        '''
-        Constructor
-        '''
+def request_url(url):
+    request = urllib2.Request(url, None, DEFAULT_HEADERS)
+    print(request.get_method() + ' ' + request.get_full_url() + ' HTTP/1.1')
+    for pair in request.header_items():
+        print(pair[0] + ": " + pair[1])
+    print
+    response = urllib2.urlopen(request)
+    print response.getcode()
+    print response.info()
     
     
